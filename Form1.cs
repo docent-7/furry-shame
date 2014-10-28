74d7333b552beb1b1ce8391ddec54f352eab1b5b
rafal <rafal0p@outlook.com>
2014-10-28 17:27:24 +0100

    First edit

--- Form1.cs
+++ Form1.cs
@@ -20,5 +20,10 @@ namespace FirstProject
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Werewolves!");
+        }
     }
 }
